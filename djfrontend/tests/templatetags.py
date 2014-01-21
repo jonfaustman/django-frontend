528d487a5ee698ee68e9b0f2b088627c924304a6
Jon Faustman <faustman@k-state.edu>
2014-01-21 16:47:32 -0600

    Test settings override default.

--- djfrontend/tests/templatetags.py
+++ djfrontend/tests/templatetags.py
@@ -2,7 +2,9 @@ from djfrontend.templatetags.settings import *
 
 from django.conf import settings
 from django.test import TestCase
+from django.test.utils import override_settings
 from django.template import Template, Context
+
 
 class DjfrontendDefaultTestCase(TestCase):
     
@@ -130,3 +132,146 @@ class DjfrontendDefaultTestCase(TestCase):
         ).render(Context())
         
         self.assertTrue(DJFRONTEND_TWBS_VERSION_DEFAULT in t)
+
+
+class DjfrontendSettingsTestCase(TestCase):
+    
+    """
+    Ensure the settings override the defaults.
+    """
+    
+    def test_djfrontend_h5bp_html(self):
+        attr = 'lang="%s"' % DJFRONTEND_H5BP_HTML_DEFAULT
+        settings.DJFRONTEND_H5BP_HTML = 'fr'
+        t = Template(
+            "{% load djfrontend %}"
+            "{% djfrontend_h5bp_html %}"
+        ).render(Context())
+        
+        self.assertFalse(attr in t)
+        
+    def test_djfrontend_h5bp_css(self):
+        settings.DJFRONTEND_H5BP_CSS = '0.0.0'
+        t = Template(
+            "{% load djfrontend %}"
+            "{% djfrontend_h5bp_css %}"
+        ).render(Context())
+    
+        self.assertFalse(DJFRONTEND_H5BP_CSS_DEFAULT in t)
+        
+    def test_djfrontend_normalize(self):
+        settings.DJFRONTEND_NORMALIZE = '0.0.0'
+        t = Template(
+            "{% load djfrontend %}"
+            "{% djfrontend_normalize %}"
+        ).render(Context())
+    
+        self.assertFalse(DJFRONTEND_NORMALIZE_DEFAULT in t)
+        
+    def test_djfrontend_fontawesome(self):
+        settings.DJFRONTEND_FONTAWESOME = '0.0.0'
+        t = Template(
+            "{% load djfrontend %}"
+            "{% djfrontend_fontawesome %}"
+        ).render(Context())
+    
+        self.assertFalse(DJFRONTEND_FONTAWESOME_DEFAULT in t)
+
+    def test_djfrontend_modernizr(self):
+        settings.DJFRONTEND_MODERNIZR = '0.0.0'
+        t = Template(
+            "{% load djfrontend %}"
+            "{% djfrontend_modernizr %}"
+        ).render(Context())
+    
+        self.assertFalse(DJFRONTEND_MODERNIZR_DEFAULT in t)
+    
+    def test_djfrontend_jquery(self):
+        settings.DJFRONTEND_JQUERY = '0.0.0'
+        t = Template(
+            "{% load djfrontend %}"
+            "{% djfrontend_jquery %}"
+        ).render(Context())
+    
+        self.assertFalse(DJFRONTEND_JQUERY_DEFAULT in t)
+    
+    def test_djfrontend_jqueryui(self):
+        settings.DJFRONTEND_JQUERYUI = '0.0.0'
+        t = Template(
+            "{% load djfrontend %}"
+            "{% djfrontend_jqueryui %}"
+        ).render(Context())
+    
+        self.assertFalse(DJFRONTEND_JQUERYUI_DEFAULT in t)
+    
+    def test_djfrontend_jquery_datatables(self):
+        settings.DJFRONTEND_JQUERY_DATATABLES = '0.0.0'
+        t = Template(
+            "{% load djfrontend %}"
+            "{% djfrontend_jquery_datatables %}"
+        ).render(Context())
+    
+        self.assertFalse(DJFRONTEND_JQUERY_DATATABLES_VERSION_DEFAULT in t)
+    
+    def test_djfrontend_jquery_datatables_css(self):
+        settings.DJFRONTEND_JQUERY_DATATABLES_CSS = '0.0.0'
+        t = Template(
+            "{% load djfrontend %}"
+            "{% djfrontend_jquery_datatables_css %}"
+        ).render(Context())
+    
+        self.assertFalse(DJFRONTEND_JQUERY_DATATABLES_VERSION_DEFAULT in t)
+    
+    def test_djfrontend_jquery_formset(self):
+        settings.DJFRONTEND_JQUERY_FORMSET = '0.0.0'
+        t = Template(
+            "{% load djfrontend %}"
+            "{% djfrontend_jquery_formset %}"
+        ).render(Context())
+    
+        self.assertFalse(DJFRONTEND_JQUERY_FORMSET_DEFAULT in t)
+    
+    def test_djfrontend_jquery_scrollto(self):
+        settings.DJFRONTEND_JQUERY_SCROLLTO = '0.0.0'
+        t = Template(
+            "{% load djfrontend %}"
+            "{% djfrontend_jquery_scrollto %}"
+        ).render(Context())
+    
+        self.assertFalse(DJFRONTEND_JQUERY_SCROLLTO_DEFAULT in t)
+    
+    def test_djfrontend_jquery_smoothscroll(self):
+        settings.DJFRONTEND_JQUERY_SMOOTHSCROLL = '0.0.0'
+        t = Template(
+            "{% load djfrontend %}"
+            "{% djfrontend_jquery_smoothscroll %}"
+        ).render(Context())
+    
+        self.assertFalse(DJFRONTEND_JQUERY_SMOOTHSCROLL_DEFAULT in t)
+    
+    def test_djfrontend_twbs_css(self):
+        settings.DJFRONTEND_TWBS_CSS = '0.0.0'
+        t = Template(
+            "{% load djfrontend %}"
+            "{% djfrontend_twbs_css %}"
+        ).render(Context())
+    
+        self.assertFalse(DJFRONTEND_TWBS_VERSION_DEFAULT in t)
+    
+    def test_djfrontend_twbs_theme_css(self):
+        settings.DJFRONTEND_TWBS_THEME_CSS = '0.0.0'
+        t = Template(
+            "{% load djfrontend %}"
+            "{% djfrontend_twbs_theme_css %}"
+        ).render(Context())
+    
+        self.assertFalse(DJFRONTEND_TWBS_VERSION_DEFAULT in t)
+    
+    def test_djfrontend_twbs_js(self):
+        settings.DJFRONTEND_TWBS_JS_VERSION = '0.0.0'
+        t = Template(
+            "{% load djfrontend %}"
+            "{% djfrontend_twbs_js %}"
+        ).render(Context())
+    
+        self.assertFalse(DJFRONTEND_TWBS_VERSION_DEFAULT in t)
