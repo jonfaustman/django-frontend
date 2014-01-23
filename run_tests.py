e61575fd94958906c327ab1c06880b33196e80af
Jon Faustman <faustman@k-state.edu>
2014-01-23 11:43:28 -0600

    Test Travis.

--- run_tests.py
+++ run_tests.py
@@ -4,11 +4,42 @@ import django
 
 DIRNAME = os.path.dirname(__file__)
 
-settings.configure(
-    INSTALLED_APPS=('django.contrib.staticfiles',
-                    'djfrontend',
-    )
-)
+if django.VERSION[1] < 4:
+    # If the version is NOT django 4 or greater
+    # then remove the TZ setting.
+
+    settings.configure(DEBUG=True,
+                       DATABASES={
+                           'default': {
+                               'ENGINE': 'django.db.backends.sqlite3',
+                               }
+                       },
+                       ROOT_URLCONF='mailqueue.urls',
+                       INSTALLED_APPS=('django.contrib.auth',
+                                       'django.contrib.contenttypes',
+                                       'django.contrib.sessions',
+                                       'django.contrib.admin',
+                                       'djfrontend',))
+else:
+    settings.configure(DEBUG=True,
+                       DATABASES={
+                           'default': {
+                               'ENGINE': 'django.db.backends.sqlite3',
+                               }
+                       },
+                       ROOT_URLCONF='mailqueue.urls',
+                       INSTALLED_APPS=('django.contrib.auth',
+                                       'django.contrib.contenttypes',
+                                       'django.contrib.sessions',
+                                       'django.contrib.admin',
+                                       'djfrontend',),
+                       USE_TZ=True)
+
+
+
 
 from django.test.simple import DjangoTestSuiteRunner
 test_runner = DjangoTestSuiteRunner(verbosity=1)
+failures = test_runner.run_tests(['djfrontend', ])
+if failures:
+    sys.exit(failures)
